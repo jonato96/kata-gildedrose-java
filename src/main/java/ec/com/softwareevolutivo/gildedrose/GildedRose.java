f0db7920a215e0f3d118ab274bd0911eae213b4e
Jonathan Sánchez <jonathan.sanchez9@zurich.com>
2024-05-26 19:05:59 -0500

    refactoring-legacy-code

--- src/main/java/ec/com/softwareevolutivo/gildedrose/GildedRose.java
+++ src/main/java/ec/com/softwareevolutivo/gildedrose/GildedRose.java
@@ -7,56 +7,80 @@ class GildedRose {
         this.items = items;
     }
 
+    private final String AGED = "Aged Brie";
+    private final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+    private final String SULFURAS = "Sulfuras, Hand of Ragnaros"; //this case doesnt need changes
+    private final String CONJURED = "Conjured";
+
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+        for (Item item : items) {            
+            if (item.name.equals(AGED)) {
+                updateAged(item);
+                continue;
             }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
+            if (item.name.equals(BACKSTAGE)) {
+                updateBackStage(item);
+                continue;
             }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+            if (item.name.equals(SULFURAS)) {                
+                continue;
             }
+            if (item.name.equals(CONJURED)) {
+                updateConjured(item);
+                continue;
+            }
+            if (item.name.equals(AGED)) {
+                updateAged(item);
+                continue;
+            }
+            updateNormal(item);
+        }  
+    }
+
+    private void updateAged(Item item) {        
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;           
+        }
+        item.sellIn -= 1;
+        if (item.sellIn < 0 && item.quality < 50) {
+            item.quality += 1;
         }
     }
+
+    private void updateBackStage (Item item) {        
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;            
+            if (item.sellIn < 11 && item.quality < 50) {
+                item.quality += 1;
+            }
+            if (item.sellIn < 6 && item.quality < 50) {
+                item.quality += 1;
+            }            
+        }
+        item.sellIn -= 1;
+        if (item.sellIn < 0) {
+            item.quality -= item.quality;
+        }
+    }    
+
+    private void updateNormal(Item item) {        
+        if (item.quality > 0) {
+            item.quality -= 1;
+        }
+        item.sellIn -= 1;
+        if (item.sellIn < 0 && item.quality > 0) {                        
+            item.quality -= 1;                        
+        }         
+    }
+
+    private void updateConjured(Item item) {        
+        if (item.quality > 0) {                
+                item.quality -= 2;                                        
+        }
+        item.sellIn -= 1;       
+        if (item.sellIn < 0 && item.quality > 0) {                                   
+            item.quality -= 1;                    
+        }             
+    }    
+
 }
